b7303a706108ea5246dd850f00c988fa15987307
Luis Vegas <luisvegasmor@gmail.com>
2023-03-07 21:32:24 -0400

    script merges and cd

--- skate.sh
+++ skate.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 INSIDE_REPO=$(git rev-parse --is-inside-work-tree 2>/dev/null)
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+CURRENT_PATH=$(pwd)
 
 if [ "$INSIDE_REPO" ]; then
-	echo "Reflex changes too:"
+	gum style \
+		--foreground 212 --border-foreground 212 --align center "That's it."
+	echo "Reflect changes to:"
 	WORKTREES=$(git worktree list | awk '{print $1}' | gum choose --no-limit)
-	gum confirm && (cd $WORKTREES) || echo "Nothing was done."
+	gum confirm "Sure to proceed?" && (cd "$WORKTREES" && git merge "$CURRENT_BRANCH" && cd "$CURRENT_PATH") || echo "Nothing was done."
 
 else
 	echo "not in git repo"
